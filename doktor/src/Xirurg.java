85e70a6e97accc99e89a83741b1bf3eba3ac7a2f
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-03 15:04:17 +0500

    add Xirurg

--- doktor/src/Xirurg.java
+++ doktor/src/Xirurg.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Xirurg {
+public class Xirurg extends Doktor{
+    private String yunalish="jarrohlik";
+    private String turlari="ichki organizimlar";
+
+
+    public void printt2(){
+
+        System.out.println(yunalish);
+    }
+
+    public void print3(){
+        System.out.println(turlari);
+    }
+    public static void main(String[] args) {
+        Xirurg xirurg=new Xirurg();
+        Doktor doktor =new Doktor();
+        System.out.println(doktor.print1());
+        System.out.println(doktor.print2());
+        xirurg.print3();
+        xirurg.printt2();
+    }
 }
