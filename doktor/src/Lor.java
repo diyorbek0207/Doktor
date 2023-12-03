95dbff851cbb9793968d49fe37c5551f7b35662f
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-03 15:01:19 +0500

    add Lor

--- doktor/src/Lor.java
+++ doktor/src/Lor.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Lor {
+public class Lor extends Doktor {
+    private String yunalish="Quloq,Burun,og'iz sohalari";
+
+    public void print(){
+        System.out.println(yunalish);
+    }
 }
