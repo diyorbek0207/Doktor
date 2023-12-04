9afa2ecf7ac1d81bab27b3fcb53189bf412ed53a
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 10:04:05 +0500

    add Electroinica

--- doktor/src/Electronica.java
+++ doktor/src/Electronica.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Electronica {
+public class Electronica {
+    private String A="Electr bilan ishlaydi";
+
+    public static void main(String[] args) {
+
+    }
 }
