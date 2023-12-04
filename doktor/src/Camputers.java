283397c68652eda5cd07bf17c8767b69af7fe7ef
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 10:03:10 +0500

    add Camputer

---
+++ doktor/src/Camputers.java
@@ -0,0 +1,7 @@
+public class Camputers extends Electronica {
+    private String B = "Kampyuter Elektrda ishlaydigan hisoblash qurulmasi";
+
+    public String kamputer() {
+        return B;
+    }
+}
